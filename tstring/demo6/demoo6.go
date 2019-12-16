1d92bf3bf08de5af60f72586bf5f0c6fe04f5abe
yuwei1 <yuweijiubayiqi@163.com>
2019-12-16 11:56:24 +0800

    字符串转成整数

--- tstring/demo6/demoo6.go
+++ tstring/demo6/demoo6.go
@@ -24,14 +24,30 @@ func strToIntTwo(str string) int32 {
 	}
 	r := []rune(str)
 	n := int32(0)
+	// fmt.Println(math.MaxInt32)
+	// fmt.Println(math.MinInt32)
 	Max_int := int32(1<<31 - 1) //math.MaxInt32
 	Min_int := int32(-1 << 31)  //math.MinInt32
+	// fmt.Println(Max_int)
+	// fmt.Println(Min_int)
 	if str[0] == '-' {
 		for i := 1; i < len(r); i++ {
 			c := int32(r[i] - '0')
-
+			// 判断是否溢出
 			if c-1 > (Max_int - n*10) {
 				n = Min_int
+				break
+			}
+			n = n*10 + c
+		}
+	} else {
+		for i := 0; i < len(r); i++ {
+			c := int32(r[i] - '0')
+			// 判断是否溢出
+			if c > (Max_int - n*10) {
+				fmt.Println(c)
+				fmt.Println(Max_int - n*10)
+				n = Max_int
 				break
 			}
 			n = n*10 + c
@@ -43,6 +59,9 @@ func strToIntTwo(str string) int32 {
 
 func main() {
 	a := "-2147483649"
+	b := "2147483698"
+
 	// fmt.Println(strToIntOne(a))
 	fmt.Println(strToIntTwo(a))
+	fmt.Println(strToIntTwo(b))
 }
