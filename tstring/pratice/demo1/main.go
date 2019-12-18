8cc8b624a9a5dc960b780e1413fe54836121c1e6
yuwei1 <yuweijiubayiqi@163.com>
2019-12-18 13:27:14 +0800

    update

--- tstring/pratice/demo1/main.go
+++ tstring/pratice/demo1/main.go
@@ -2,8 +2,6 @@ package main
 
 import "strings"
 
-import "fmt"
-
 // 对字符串按照空格进行切割,存放到数组中，对数组进行反转
 func ResverString(str *string) {
 	strs := strings.Split(*str, " ")
@@ -17,30 +15,6 @@ func ResverString(str *string) {
 		j -= 1
 	}
 	*str = strings.Join(strs, " ")
-}
-func cou(a []int, n int) {
-	if n < 0 {
-		return
-	}
-	fmt.Println("执行次数", a[n])
-	if n > 1 {
-		cou(a, n-1)
-	}
-}
-
-func bsearch1(a []int, e int, left int, right int) int {
-	if left > right {
-		return -1
-	}
-	mid := (left + right) / 2
-	if a[mid] == e {
-		return mid
-	} else if a[mid] > e {
-		return bsearch1(a, e, left, mid-1)
-	} else {
-		return bsearch1(a, e, mid+1, right)
-	}
-	return -1
 }
 func main() {
 	// str := "I am a student."
